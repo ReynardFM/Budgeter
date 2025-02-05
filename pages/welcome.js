c0c551d98fabaadf0f592f39483590bae87cc31c
ReynardFM <reyfitzm@gmail.com>
2025-02-05 01:43:26 -0500

    Update welcome.js

--- pages/welcome.js
+++ pages/welcome.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { Layout } from "./layout";
+import Layout from "./layout";
 import Link from "next/link";
 
 export default function Welcome() {
